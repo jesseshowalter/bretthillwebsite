c5e0e7358100b66320edeb6c731fa225905621bc
Jesse Showalter <iamjesseshowalter@gmail.com>
2016-08-31 16:17:29 -1000

    installed responsive fading slider

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -35,13 +35,19 @@ $(document).ready(function(){
   // --------------------------------------------
   // homepage slider
   // --------------------------------------------
-  $(function(){
-    $('#slider').rbtSlider({
-      height: '100vh',
-      'dots': true,
-      'arrows': true,
-      'auto': 8
-    });
+
+  $("#slider4").responsiveSlides({
+    auto: false,
+    pager: false,
+    nav: true,
+    speed: 500,
+    namespace: "callbacks",
+    before: function () {
+      $('.events').append("<li>before event fired.</li>");
+    },
+    after: function () {
+      $('.events').append("<li>after event fired.</li>");
+    }
   });
 
   // --------------------------------------------
