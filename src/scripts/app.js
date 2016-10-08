754ff9d18f9bc2dc3be65c8a7bf84fec88c77133
Jesse Showalter <iamjesseshowalter@gmail.com>
2016-10-07 23:00:49 -1000

    auto rotate the homepage slider
    6500

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -44,23 +44,6 @@ $(document).ready(function(){
     });
   });
 
-  // --------------------------------------------
-  // homepage slider
-  // --------------------------------------------
-  //
-  // $("#slider4").responsiveSlides({
-  //   auto: true,
-  //   pager: false,
-  //   nav: true,
-  //   speed: 500,
-  //   namespace: "callbacks",
-  //   before: function () {
-  //     $('.events').append("<li>before event fired.</li>");
-  //   },
-  //   after: function () {
-  //     $('.events').append("<li>after event fired.</li>");
-  //   }
-  // });
 
   // --------------------------------------------
   // wow animations
